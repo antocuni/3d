8ceeb7fc29ddbfc77514c1f180e7cf34139d6e29
Antonio Cuni <anto.cuni@gmail.com>
2021-12-16 17:13:56 +0100

    new version of cura

--- cura.sh
+++ cura.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
+
 #QT_SCALE_FACTOR=1.5 ./Ultimaker_Cura-4.8.0.AppImage
-QT_SCALE_FACTOR=1.5 ./Ultimaker_Cura-4.12.0.AppImage
+#QT_SCALE_FACTOR=1.5 ./Ultimaker_Cura-4.12.0.AppImage "$@"
+QT_SCALE_FACTOR=1.5 ./Ultimaker_Cura-4.12.1.AppImage "$@"
